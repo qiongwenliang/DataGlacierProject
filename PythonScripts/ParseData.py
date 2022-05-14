54fcbcc4b312905939096c3d2a4ba82385ca72dd
Caterpillar <caterpillar902@gmail.com>
2022-05-14 00:05:30 -0700

    Add ParseData and Test

---
+++ PythonScripts/ParseData.py
@@ -0,0 +1,32 @@
+import csv
+import numpy as np
+
+
+class ParseData:
+    def __init__(self, path):
+        """
+        This class is to parse csv document and put it into python data structures that is ready to be used, remove
+         duplicates, and check NA value at the same time.
+        :param path: path to the csv file.
+        """
+        file_obj = open(path, 'r')
+        reader = csv.reader(file_obj)
+
+        self.raw_data = []
+
+        for row in reader:
+            self.raw_data.append(tuple(row))
+
+        self.title = self.raw_data[0]
+
+        self.raw_data = set(self.raw_data[1:])
+
+        self.data_body = []
+
+        for data in self.raw_data:
+            data_list = list(data)
+            for i in range(len(data_list)):
+                if (isinstance(data_list[i], float) or isinstance(data_list[i], int)) and np.isnan(data_list[i]):
+                    break
+                elif i == len(data_list) - 1:
+                    self.data_body.append(data_list)
---
+++ PythonScripts/Test.pyi
@@ -0,0 +1,2 @@
+file = ParseData("C:/Users/cater_yrqwkk6/Repository/DataSets/City.csv")
+print(file.data_body)
